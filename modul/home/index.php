2855709ff14fd7b94a846f4542c7eba3395e678f
Elisiyaputri <pelisya30@gmail.com>
2024-03-16 04:07:14 -0700

    Menambahkan folder modul yang berisi file akun.php

---
+++ modul/home/index.php
@@ -0,0 +1,4 @@
+<p class="alert text-dark bg-primary bg-opacity-25">
+    Selamat datang di aplikasi <strong>Sistem Informasi Akuntansi</strong>,
+    Aplikasi ini dirancang untuk memudahkan manajemen transaksi penjualan
+</p>
